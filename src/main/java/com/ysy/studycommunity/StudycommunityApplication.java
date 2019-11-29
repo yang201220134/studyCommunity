d309fa26212e6935d97b9cdd6903002418a52615
unknown <chuangyeweiwang@icloud.com>
2019-11-29 09:11:36 +0800

    first commit

---
+++ src/main/java/com/ysy/studycommunity/StudycommunityApplication.java
@@ -0,0 +1,13 @@
+package com.ysy.studycommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudycommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudycommunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ysy/studycommunity/StudycommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ysy.studycommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudycommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
